7e9f3d6ea4a2b00d96ee48ce325e989dc859d94c
basem909 <basemshams30@gmail.com>
2022-12-07 11:38:40 -0300

    Add validations to the transactions model

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,9 @@
+class TransactionsController < ApplicationController
+  validates :transaction_id, presence: true
+  validates :merchant_id, presence: true
+  validates :user_id, presence: true
+  validates :card_number, presence: true
+  validates :transaction_date, presence: true
+  validates :transaction_amount, presence: true
+  validates :has_cbk, presence: true
+end
