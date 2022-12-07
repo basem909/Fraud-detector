5ba67fd6a770d79df4006915ec90228855d0f661
basem909 <basemshams30@gmail.com>
2022-12-07 12:03:30 -0300

    Set up the route for create action

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,3 +1,10 @@
 class TransactionsController < ApplicationController
+  def create
+
+  end
   
+  private
+  def transaction_params
+     params.permit(:transaction_id, :merchant_id, :user_id, :card_number, :transaction_date, :transaction_amount, )
+  end
 end
